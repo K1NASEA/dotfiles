93594b0f4a19e1d0ab6812d7c53b41c95edf546c
K1NASEA <44296719+K1NASEA@users.noreply.github.com>
2024-04-25 09:51:03 +0000

    fix apt install.sh

--- config/apt/install.sh
+++ config/apt/install.sh
@@ -5,10 +5,6 @@ set -u  # Treat unset variables as errors
 
 . /etc/os-release
 
-if [ "$ID" = "ubuntu" ]; then
-	add-apt-repository -y ppa:git-core/ppa
-fi
-
 apt-get update
 apt-get upgrade -y
 apt-get install -y \
@@ -17,13 +13,12 @@ apt-get install -y \
 	curl \
 	dnsutils \
 	file \
-	git \
-	git-lfs \
 	htop \
 	jq \
 	openssl \
 	procps \
 	shellcheck \
+	software-properties-common \
 	telnet \
 	unzip \
 	wget \
@@ -31,10 +26,19 @@ apt-get install -y \
 	zip \
 	zsh
 
+# git
+if [ "$ID" = "ubuntu" ]; then
+	add-apt-repository -y ppa:git-core/ppa
+fi
+apt-get update
+apt-get install -y \
+	git \
+	git-lfs
+
 # Docker
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL "https://download.docker.com/linux/$ID/gpg" -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$ID $VERSION_CODENAME stable"
+add-apt-repository -y "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$ID $VERSION_CODENAME stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
