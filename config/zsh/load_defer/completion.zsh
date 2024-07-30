879aab36268bedacbead5536147df185d191b9d7
K1NASEA <44296719+K1NASEA@users.noreply.github.com>
2024-07-30 10:39:33 +0900

    fix completion

--- config/zsh/load_defer/completion.zsh
+++ config/zsh/load_defer/completion.zsh
@@ -1,3 +1,7 @@
+fpath=(
+    $fpath
+    ${ZDOTDIR}/site-functions
+)
 [ ! -d "${XDG_CACHE_HOME}/zsh" ] && mkdir -p "${XDG_CACHE_HOME}/zsh"
 autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump-$ZSH_VERSION"
 zstyle ':completion:*' cache-path "${XDG_CACHE_HOME}/zsh/zcompcache"
